ce2133b20ed1834118f1c2b1b548d405461e9226
Ilya Merkushev <servercs50@gmail.com>
2019-03-19 23:52:39 +0300

    add blind color filter on document ready

--- content.js
+++ content.js
@@ -2,3 +2,33 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.log(request.action);
     });
+
+$(document).ready(function () {
+    // let images = $('img');
+    // console.log(images);
+    // images.each(function () {
+    //     console.log($(this)[0].clientWidth + " " + $(this)[0].clientHeight);
+    // });
+    function getRGB(str){
+        let match = str.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
+        return [match[1], match[2], match[3]];
+    }
+
+    let deffect_type = "protanopia";
+
+    $("*").each(function () {
+        let rgb = getRGB($(this).css("background-color"));
+        let blind_rgb;
+        if (JSON.stringify(rgb) !== JSON.stringify(["0", "0", "0"])) {
+            if (deffect_type === "protanopia") {
+                blind_rgb = fBlind.Protanopia(rgb);
+            } else if (deffect_type === "deuteranopia") {
+                blind_rgb = fBlind.Deuteranopia(rgb);
+            } else if (deffect_type === "tritanopia") {
+                blind_rgb = fBlind.Tritanopia(rgb);
+            }
+            blind_rgb = blind_rgb.join(", ");
+            $(this).css({"background-color": "rgb(" + blind_rgb + ")"})
+        }
+    });
+});
