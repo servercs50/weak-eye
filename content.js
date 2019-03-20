3f20528bc25c3573c93be038d19963432160314f
Ilya Merkushev <servercs50@gmail.com>
2019-03-20 14:19:51 +0300

    Add new filters, fix bug when append filter to the body

--- content.js
+++ content.js
@@ -1,32 +1,33 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(request);
+        console.log(request.defect);
         blindFilter(request.defect);
     });
 
+let colorMatrix = {
+    "deuteranopia": '0.567 0.433 0 0 0    0.558 0.442 0 0 0      0 0.242 0.758 0 0      0 0 0 1 0',
+    "protanopia":   '0.625 0.375 0 0 0    0.7 0.3 0 0 0        0 0.3 0.7 0 0        0 0 0 1 0',
+    "tritanopia":   '0.95 0.05 0 0 0      0 0.433 0.567 0 0      0 0.475 0.525 0 0      0 0 0 1 0',
+};
 
 let blindFilter = function(defectType) {
     $('body').append(
-        "<div id='filterBlock' visibility='visible'>" +
-            "<svg xmlns='http://www.w3.org/2000/svg' version='1.1'>" +
-                "<defs>" +
-                    "<filter id=" + defectType + ">" +
-                    "</filter>" +
-                "</defs>" +
-            "</svg>" +
+        "<div class='filterBlock' style='visibility: visible'>" +
+        "<svg xmlns='http://www.w3.org/2000/svg' version='1.1'>" +
+        "<defs>" +
+        "<filter id=" + defectType + ">" +
+        "<feColorMatrix type=\"matrix\" values=\"" + colorMatrix[defectType] + "\" />'" +
+        "</filter>" +
+        "</defs>" +
+        "</svg>" +
         "</div>"
     );
-    let feColorMatrixElement = $('<feColorMatrix>', {type: 'matrix',
-        values: '0.95 0.05 0 0 0' +
-                '0 0.433 0.567 0 0' +
-                '0 0.475 0.525 0 0' +
-                '0 0 0 1 0'
-    });
-    $('#' + defectType).append(feColorMatrixElement);
-    $("body").css({'filter': 'url("#' + defectType + '")'});
-    $("body").hide().show(0);
+
+    $("html").css('cssText', 'filter: url("#' + defectType + '")');
+
+    return defectType
 };
 
 $(document).ready(function () {
-    blindFilter("deuteranopia");
+    // let filterElementId = blindFilter("tritanopia");
 });
