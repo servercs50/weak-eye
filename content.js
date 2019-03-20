25498c94272966c327804d42cd16d526d9216921
Ilya Merkushev <servercs50@gmail.com>
2019-03-20 17:07:38 +0300

    More filters and comments

--- content.js
+++ content.js
@@ -1,16 +1,33 @@
+// Слушаем сообщения от popup.js
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.log(request.defect);
         blindFilter(request.defect);
     });
 
+// коэффициенты матрицы фильтра взяты с https://github.com/eeejay/NoCoffee
 let colorMatrix = {
-    "deuteranopia": '0.567 0.433 0 0 0    0.558 0.442 0 0 0      0 0.242 0.758 0 0      0 0 0 1 0',
-    "protanopia":   '0.625 0.375 0 0 0    0.7 0.3 0 0 0        0 0.3 0.7 0 0        0 0 0 1 0',
-    "tritanopia":   '0.95 0.05 0 0 0      0 0.433 0.567 0 0      0 0.475 0.525 0 0      0 0 0 1 0',
+    "deuteranopia": '0.567 0.433 0 0 0\n' +
+                    '0.558 0.442 0 0 0\n' +
+                    '0 0.242 0.758 0 0\n' +
+                    '0 0 0 1 0',
+    "protanopia":   '0.625 0.375 0 0 0\n' +
+                    '0.7 0.3 0 0 0\n' +
+                    '0 0.3 0.7 0 0\n' +
+                    '0 0 0 1 0',
+    "tritanopia":   '0.95 0.05 0 0 0\n' +
+                    '0 0.433 0.567 0 0\n' +
+                    '0 0.475 0.525 0 0\n' +
+                    '0 0 0 1 0',
+    "achromotopsia":'0.299 0.587 0.114 0 0\n' +
+                    '0.299 0.587 0.114 0 0\n' +
+                    '0.299 0.587 0.114 0 0\n' +
+                    '0 0 0 1 0',
+    "nofilter": null,
 };
 
 let blindFilter = function(defectType) {
+    // применяем фильтр к странице, добавляя элемент в разметку
     $('body').append(
         "<div class='filterBlock' style='visibility: visible'>" +
         "<svg xmlns='http://www.w3.org/2000/svg' version='1.1'>" +
@@ -22,12 +39,6 @@ let blindFilter = function(defectType) {
         "</svg>" +
         "</div>"
     );
-
+    // и применяя стили к html
     $("html").css('cssText', 'filter: url("#' + defectType + '")');
-
-    return defectType
 };
-
-$(document).ready(function () {
-    // let filterElementId = blindFilter("tritanopia");
-});
--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,12 @@
 let btns = document.getElementsByClassName("defect-name");
 for (let i = 0; i < btns.length; i++) {
+    // Вешаем обработчики клика на кнопки
     btns[i].addEventListener('click', function (e) {
+        // Выбираем для изменений только активную вкладку
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            // Определяем тип фильтра исходя из id нопки
             let defectType = e.target.id;
+            // Отправляем тип фильтра скрппту страницы
             chrome.tabs.sendMessage(tabs[0].id, {defect: defectType}, function (response) {
             });
         });
